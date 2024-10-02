55a2af5a0f4cea504ecab69a9d8f87c27d438c69
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-02 11:26:16 -0300

    feat: add DeliveryOrder interface

---
+++ packages/core/src/order/DeliveryOrder.ts
@@ -0,0 +1,10 @@
+export default interface PedidoEntrega {
+  id: number;
+  name: string;
+  email: string;
+  document: string;
+  street_address: string;
+  complement: string;
+  city: string;
+  state: string;
+}
